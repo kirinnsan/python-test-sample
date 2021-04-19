e492bc446be59d437d3f904501b3ddf57a686117
Hiroshi Takaichi <hirosi.t822@gmail.com>
2021-04-20 00:37:27 +0900

    Add sample project

---
+++ bopytest-code/code/tasks_proj/tests/conftest.py
@@ -0,0 +1,7 @@
+"""Placeholder."""
+
+# nothing here yet
+import sys 
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + "/../src/"))
---
+++ bopytest-code/code/tasks_proj/tests/unit/test_task.py
@@ -0,0 +1,23 @@
+# import os
+# from tasks import Task
+# print(os.path)
+
+
+# def test_aaa():
+#     t1 = Task('sit there', 'brian')
+#     t2 = Task('do something', 'okken')
+#     assert t1 == t2
+
+
+# def test_dict_equality():
+#     t1_dict = Task('make sandwich', 'okken')._asdict()
+#     t2_dict = Task('make sandwich', 'okkem')._asdict()
+#     assert t1_dict == t2_dict
+
+
+# def aaa_test():
+#     pass
+
+
+# def test_add():
+#     pass
